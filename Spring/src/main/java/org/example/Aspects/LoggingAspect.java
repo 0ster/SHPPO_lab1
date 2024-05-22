27258c711fe109a9dbbe8842d2bdd34fa4890b3d
Kopparka <eser_eser04@mail.ru>
2024-05-22 22:02:27 +0300

    Spring (IoC,DI,AOP) 1.0

--- Spring/src/main/java/org/example/Aspects/LoggingAspect.java
+++ Spring/src/main/java/org/example/Aspects/LoggingAspect.java
@@ -1,6 +1,7 @@
 package org.example.Aspects;
 
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,11 @@ public class LoggingAspect {
         System.out.println("После выполнения метода subscribe");
     }
 
+    @AfterReturning("execution(* org.example.Publisher_Subscribe.ContextMenu.subscribe(..))")
+    public void afterReturningSubscribeAdvice() {
+        System.out.println("After successfully subscribing");
+    }
+
     @Before("execution(* org.example.Publisher_Subscribe.ContextMenu.unsubscribe(..))")
     public void logBeforeUnsubscribe() {
         System.out.println("Перед выполнением метода unsubscribe");
@@ -27,6 +33,11 @@ public class LoggingAspect {
     @After("execution(* org.example.Publisher_Subscribe.ContextMenu.unsubscribe(..))")
     public void logAfterUnsubscribe() {
         System.out.println("После выполнения метода unsubscribe");
+    }
+
+    @AfterReturning("execution(* org.example.Publisher_Subscribe.ContextMenu.unsubscribe(..))")
+    public void afterReturningUnsubscribeAdvice() {
+        System.out.println("After successfully unsubscribing");
     }
 
     @Before("execution(* org.example.Factory_SingleTon_Composite.CompositeMenuItem.addChild(..))")
@@ -47,5 +58,34 @@ public class LoggingAspect {
     @After("execution(* org.example.Factory_SingleTon_Composite.CompositeMenuItem.removeChild(..))")
     public void logAfterRemoveChild() {
         System.out.println("После выполнения метода removeChild");
+    }
+
+    @Before("execution(* org.example.Publisher_Subscribe.ContextMenu.notifySubscribers(..))")
+    public void logBeforeNotifySubscribers() {
+        System.out.println("Перед выполнением метода notifySubscribers");
+    }
+
+    @After("execution(* org.example.Publisher_Subscribe.ContextMenu.notifySubscribers(..))")
+    public void logAfterNotifySubscribers() {
+        System.out.println("После выполнения метода notifySubscribers");
+    }
+
+    @AfterReturning("execution(* org.example.Publisher_Subscribe.ContextMenu.notifySubscribers(..))")
+    public void afterReturningNotifySubscribersAdvice() {
+        System.out.println("After successfully notifying subscribers");
+    }
+    @Before("execution(* org.example.Publisher_Subscribe.ContextMenu.setState(..))")
+    public void logBeforeSetState() {
+        System.out.println("Перед выполнением метода setState");
+    }
+
+    @After("execution(* org.example.Publisher_Subscribe.ContextMenu.setState(..))")
+    public void logAfterSetState() {
+        System.out.println("После выполнения метода setState");
+    }
+
+    @AfterReturning("execution(* org.example.Publisher_Subscribe.ContextMenu.setState(..))")
+    public void afterReturningSetStateAdvice() {
+        System.out.println("After successfully setting state");
     }
 }
